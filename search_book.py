cd8a7e475786192ddb0d3d869e23520565c36b66
vinson <vinsonring@gmail.com>
2016-05-26 16:26:02 +0800

    update

--- search_book.py
+++ search_book.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-# coding:utf-8
+#coding:utf-8
 
 import requests
-import urllib2
+import urllib2,urllib
 from bs4 import BeautifulSoup
 import re
 import time
@@ -17,21 +17,32 @@ TITLE = 'a'
 PageNum = 20
 
 # 书目查询
-API = '/cgi-bin/IlaswebBib?v_index=TITLE&v_value=' + TITLE + '&FLD_DAT_BEG=&FLD_DAT_END=&v_pagenum=' + unicode(
-    PageNum) + '&v_seldatabase=0&v_LogicSrch=0'
+API = '/cgi-bin/IlaswebBib'
 
-search_book_html = urllib2.urlopen(LIB_HOST + API)
-search_book_Obj = BeautifulSoup(search_book_html, "html.parser", from_encoding="GB18030")
+TITLE = raw_input().decode('utf-8')
 
-for tr in search_book_Obj.find("table", {"bgcolor": "#008080"}).tr.next_siblings:
-    try:
-        td = tr.find_all("td")
-        print u"题名:" + td[0].get_text() \
-                + u" 作者:" + td[1].get_text() \
-                + u" 出版社:" + td[2].get_text() \
-                + u" 页码:" + td[3].get_text() \
-                + u" 价格:" + td[4].get_text() \
-                + u" 索引号:" + td[5].get_text() \
-                + u" Book_Link: " + td[6].a.attrs['href']
-    except:
-        pass
+url = LIB_HOST + API
+
+values = {
+        'v_index' : 'TITLE',
+        'v_value' : TITLE.encode('gb2312'),
+        'v_pagenum' : '30',
+        }
+
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data)
+new_book_html = urllib2.urlopen(req)
+new_book_Obj = BeautifulSoup(new_book_html,"html.parser",from_encoding="GB18030")
+
+for tr in new_book_Obj.find("table",{"bgcolor":"#008080"}).tr.next_siblings:
+        try:
+            td= tr.find_all("td")
+            print   u"题名:" + td[0].get_text() \
+                        + u" 作者:" + td[1].get_text() \
+                        + u" 出版社:" + td[2].get_text() \
+                        + u" 页码:" + td[3].get_text() \
+                        + u" 价格:" + td[4].get_text() \
+                        + u" 索引号:" + td[5].get_text() \
+                        + u" Book_Link: " + td[6].a.attrs['href']
+        except: 
+            pass
